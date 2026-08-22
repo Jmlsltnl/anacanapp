4caee968bc5c7547b9bd727004450e07ae234a9c
jamilsultanli <jmlsltnl@gmail.com>
2026-08-22 22:44:47 +0400

    chore(ios): remove orphaned old Plugins/ folder (LiveActivityPlugin.swift+.m, superseded by properly-targeted copy at ios/App/App/LiveActivityPlugin.swift)

--- ios/App/App/Plugins/LiveActivityPlugin.m
+++
@@ -1,8 +0,0 @@
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(LiveActivityPlugin, "LiveActivity",
-    CAP_PLUGIN_METHOD(startActivity, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(stopActivity, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(getPendingStops, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(clearPendingStops, CAPPluginReturnPromise);
-)
